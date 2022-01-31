1d15a5d67b89644cf3a3c2c934d1ff9dff81fd7e
aishaalii <92413191+aishaalii@users.noreply.github.com>
2022-01-30 23:33:03 -0600

    added

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
+var currentHour = moment().format('h:mm:ss a');
+
+
+document.ready(function () {
+    saveBtn.on("click, functioni () {
+        var text = (this).sin
+    }
+}
